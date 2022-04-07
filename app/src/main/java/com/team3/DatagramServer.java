095878362faf75f7d47054380c0e877a78c44b89
indorif0227 <indorif0227@gmail.com>
2022-04-07 15:59:58 -0500

    Removed client code from DatagramServer class

--- app/src/main/java/com/team3/DatagramServer.java
+++ app/src/main/java/com/team3/DatagramServer.java
@@ -2,47 +2,22 @@ package com.team3;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.nio.ByteBuffer;
 
 class DatagramServer {
-    private DatagramSocket sender;
     private DatagramSocket receiver;
 
     DatagramServer(int receiverPort, int senderPort) throws Exception {
-        // Create and assign ports for two DatagramSockets that will handle sending and
-        // receiving data
-        this.sender = new DatagramSocket(senderPort);
+        // Create a receiver for, well, receiving packets
         this.receiver = new DatagramSocket(receiverPort);
 
-        // Define IP address to send UDP packet to (currently the local machine)
-        InetAddress address = InetAddress.getLocalHost();
+        while (true) {
+            // Sample data packet to receive
+            byte[] receivedData = new byte[32];
+            DatagramPacket receivedPacket = new DatagramPacket(receivedData, receivedData.length);
 
-        // Sample integer to send
-        int sampleData = 2043;
-        // Convert int to byte array
-        ByteBuffer buffer = ByteBuffer.allocate(4);
-        buffer.putInt(sampleData);
-        // Create sample data packet to send
-        DatagramPacket packetOut = new DatagramPacket(buffer.array(), buffer.array().length, address, receiverPort);
-
-        // Sample data packet to receive (idk how large the data will be yet)
-        byte[] dataIn = new byte[4];
-        DatagramPacket packetIn = new DatagramPacket(dataIn, dataIn.length);
-
-        // Testing if data is even being received by the port
-        this.sender.send(packetOut);
-        this.receiver.receive(packetIn);
-
-        // Convert byte arrays back to integers to see if the data was sent properly
-        ByteBuffer packetOutBuffer = ByteBuffer.allocate(4);
-        packetOutBuffer.put(packetOut.getData());
-        packetOutBuffer.rewind();
-        System.out.println("[+] Data sent from sender socket: "+ packetOutBuffer.getInt());
-
-        ByteBuffer packetInBuffer = ByteBuffer.allocate(4);
-        packetInBuffer.put(packetIn.getData());
-        packetInBuffer.rewind();
-        System.out.println("[+] Data received by receiver socket: " + packetInBuffer.getInt());
+            // Testing if data is being received by our socket
+            this.receiver.receive(receivedPacket);
+            System.out.println(new String(receivedData));
+        }
     }
 }
